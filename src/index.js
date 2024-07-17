5fbbf78a1757177ea5f84fa75388d17777339c00
Demetrius <homster100@gmail.com>
2024-07-17 18:30:22 +0300

    almost complited

--- src/index.js
+++ src/index.js
@@ -28,9 +28,18 @@ function removeCard(evt) {
     evt.target.closest('.places__item').remove();
 };
 
+//функция лайка карточки и разлайка
+function giveLike(evt) {
+   if (evt.target.classList.contains('card__like-button_is-active')){
+    evt.target.classList.remove('card__like-button_is-active')
+   }else {
+    evt.target.classList.add('card__like-button_is-active')
+   }
+}
+
 //Вывести карточки на страницу
 initialCards.forEach(function(card){
-    const item = addCards (card, removeCard);
+    const item = addCards (card, removeCard, giveLike, showCard);
     place.append(item); 
 });
 
@@ -40,7 +49,7 @@ formAddCards.addEventListener('submit', (evt) => {
     const titleOfCard = document.querySelector('.popup__input_type_card-name');
     const ancorOfCard = document.querySelector('.popup__input_type_url');
     const newCard = {name: titleOfCard.value, link: ancorOfCard.value}
-    const item = addCards (newCard, removeCard);
+    const item = addCards (newCard, removeCard, giveLike);
     place.prepend(item);
     closePopup(addPopup);
     titleOfCard.value = "";
@@ -57,7 +66,13 @@ addButton.addEventListener('click', () =>{
     openWindow(addPopup);
 });
 
-
+function showCard (card){
+    const imageInCard = document.querySelector('.popup__image');
+    const titleInCard = document.querySelector('.popup__caption');
+    imageInCard.src = card.link
+    titleInCard.textContent = card.name
+    openWindow(imagePopup);
+}
 
 
 //профиль
--- src/scripts/card.js
+++ src/scripts/card.js
@@ -2,18 +2,25 @@
 import {cardTemplate, openWindow, imagePopup} from "../index.js";
 
 // @todo: Функция создания карточки
-function addCards (card , removeCard) {
+function addCards (card, removeCard, giveLike, showCard) {
     // клонируем содержимое тега template
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     // создаём узел кнопки удадения
     const deleteButton = cardElement.querySelector('.card__delete-button');
+    //создаём узел кнопки like
+    const likeButton = cardElement.querySelector('.card__like-button')
     // наполняем содержимым
     cardElement.querySelector('.card__image').src = card.link;
     cardElement.querySelector('.card__image').alt = card.link;
     cardElement.querySelector('.card__title').textContent = card.name;
+    //добавляем обрабочик кнопки удаления
     deleteButton.addEventListener('click', removeCard);
+    //добавляем обрабочик кнопки like
+    likeButton.addEventListener('click', giveLike);
+
     const cardButton = cardElement.querySelector('.card__image');
-    cardButton.addEventListener('click', () =>{openWindow(imagePopup)}); //обработчик открытия попапа карточки
+    //обработчик открытия попапа карточки
+    cardButton.addEventListener('click', () =>showCard(card));
     return cardElement;  
 };
 
